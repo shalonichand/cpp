84871afe06f1bda6e7d540783587388f2c587a18
shalonichand <shaloni.chand@outlook.com>
2024-08-06 10:56:45 +0530

    Day 0

---
+++ basic_practice/sum_of_2_num.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, sum;
+    a = 10;
+    b = 20;
+    sum = a + b;
+    cout << "The sum of two no is: " << sum;
+    return 0;
+}
